6616e6867e19a2ed7bef74bcb1e7477a07e46053
micymike <mosesmichael878@gmail.com>
2025-04-28 13:00:28 +0300

    added port number

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // specify default port
+    host: true, // listen on all addresses
+    strictPort: true, // fail if port is already in use
+  }
 })
